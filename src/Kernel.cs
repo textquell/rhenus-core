a2f3380a04bf2e90f7cab7471b001dd4c3f4a732
HaMster21 <hamster.of.dev@gmail.com>
2012-12-27 18:15:16 +0100

    Writing the initial kernel
    This first implementation of the kernel is not really doing anything.
    When the main method inside the kernel class is run it creates a kernel
    instance. This sets up logging and Settings, and is looking for the
    config file. When found it sets the internal property for the
    threadcount to this value, otherwise a default value is used.

--- src/Kernel.cs
+++ src/Kernel.cs
@@ -1,9 +1,51 @@
-﻿namespace Rhenus.Core
+﻿using Rhenus.Core.API;
+using Rhenus.Core.Properties;
+
+using log4net;
+
+namespace Rhenus.Core
 {
     class Kernel
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Kernel kernel = new Kernel();
+                kernel.SetUp();
+            }
+            catch ( System.Exception exception )
+            {
+                // TODO: Make any user-visible messages localizable
+                System.Console.WriteLine( "Exception on booting the Kernel" );
+                System.Console.WriteLine( "Exception was " + exception.Message);
+            }
+        }
+
+        #region HelperClasses
+        readonly Settings settings = new Settings();
+        readonly ITaskScheduler taskScheduler;
+        readonly ILog KernelLogger = LogManager.GetLogger( "Rhenus.Core.Kernel" );
+        #endregion
+
+        #region DefaultValues
+        public readonly int DEFAULTTHREADCOUNTPROPERTY = 4;
+        #endregion
+
+        #region CurrentSettings
+        int ThreadCount { get; set; }
+        #endregion
+
+        void SetUp ()
+        {
+            // TODO: Set up logging for this class before anything happens. Use and configure log4net to do so.
+
+            // check if ThreadCount setting is configurated
+            if (settings.ThreadCount.Equals(null)) ThreadCount = DEFAULTTHREADCOUNTPROPERTY;
+            else ThreadCount = settings.ThreadCount;
+
+            // TODO: implement ITaskScheduler interface and instantiate the class here
+            //taskScheduler = new TaskScheudler();
         }
     }
 }
